219628e4df96308177b0cbaead1bf9573a82d519
Glanfaloth Cevnas <tyl15@tsinghua.org.cn>
2021-01-09 12:50:43 +0100

    image map

---
+++ js/name-map.js
@@ -0,0 +1,8 @@
+function displayName(x) {
+    document.getElementById(x.alt).style.visibility = "visible";
+    document.getElementById(x.alt).style.opacity = "1";
+}
+function hideName(x) {
+    document.getElementById(x.alt).style.visibility = "hidden";
+    document.getElementById(x.alt).style.opacity = "0";
+}
